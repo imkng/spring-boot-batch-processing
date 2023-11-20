6decb24c2d91a1c2dc18421b49adb440d11d92cb
krnand <1india.kng@gmail.com>
2023-11-20 23:34:31 +0530

    initial commits

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS products(
+    product_id int primary key,
+    title varchar(20),
+    description varchar(200),
+    price varchar(10),
+    discount varchar(2),
+    discounted_price varchar(10)
+);
